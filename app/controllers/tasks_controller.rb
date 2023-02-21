7021fbd2b27f51835268c721e653376118676f82
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-02-21 14:59:48 +0100

    ✨ Gérer une liste de choses en rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+
+  def index # Read all
+    @tasks = Task.all
+  end
+
+  def new # Create
+    @task = Task.new
+  end
+
+  def create # Create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def show # Show
+    @task = Task.find(params[:id])
+  end
+
+  def edit # Update
+    @task = Task.find(params[:id])
+  end
+
+  def update # Update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy # Delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def done # Done
+    @task = Task.find(params[:id])
+    @task.update(completed: true)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "tasks#index"
+  # Read all
+  get    "tasks", to: "tasks#index"
+  # Create
+  get    "tasks/new", o: "tasks#new",  as: :new_task
+  post   "tasks", to: "tasks#create"
+  # Show
+  get    "tasks/:id", to: "tasks#show", as: :task
+  # Update
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch  "tasks/:id", to: "tasks#update"
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
+  # Done
+  post "tasks/:id/done", to: "tasks#done", as: :done_task
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
